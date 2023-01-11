7d5a5e528ed5054bb01ee24ab39e7ea18c1ffcff
Serhii Burdeinyi <tibtus@gmail.com>
2023-01-11 16:25:57 +0200

    #update_project_v_4

--- src/components/members/members.jsx
+++ src/components/members/members.jsx
@@ -14,10 +14,17 @@ const Members = () => {
 
             </h1>
             <div className="position">UDEMY</div>
-            <h3 className="period"> 04.2021 - 08.2021 (4 months) </h3>
-            {/* <h4 className="about">
-              - text
-            </h4>*/}
+            <h3 className="period"> 04.2021 - 08.2021 ( 4 months ) </h3>
+            <h4 className="about">
+              HTML5 | CSS3 |
+              Bootstrap 4/5 | FlexBox | CSS Grid |
+              Mobile adaptation of sites and applications |
+              Landing sites on CMS |
+              Site speed optimization |
+              Sass/Scss/Less preprocessors |
+              BEM methodology |
+              Automating processes using the Gulp task scheduler |
+            </h4>
             <a href="#contact" className="contact-member">
               <span>Certificate</span>
             </a>
@@ -30,11 +37,22 @@ const Members = () => {
             <h1 className="name">Course "Complete JavaScript + React"</h1>
             <h3 className="position">UDEMY</h3>
             <h3 className="period">
-              08.2021 - 04.2022 (8 months)
+              08.2021 - 04.2022 ( 8 months )
             </h3>
-            {/*<h4 className="about">
-              - text
-            </h4>*/}
+            <h4 className="about">
+              ES6+ | technologies AJAX, JSON |
+              JSX, Error Boundaries |
+              Render-props pattern |
+              React + Redux |
+              npm, Babel, Browserify, Webpack, Heroku, Firebase |
+              React (Redux) |
+              Git and GitHub |
+              Promise (ES6) |
+              Fetch API |
+              Async/Await (ES8) |
+              Jquery library |
+              JS animation, requestAnimationFrame |
+            </h4>
             <a href="#contact" className="contact-member">
               <span>Certificate</span>
             </a>
@@ -47,11 +65,14 @@ const Members = () => {
             <h1 className="name">Course "CMS WordPress"</h1>
             <h3 className="position">UDEMY</h3>
             <h3 className="period">
-              08.2021 - 09.2021 (1 month)
+              08.2021 - 09.2021 ( 1 month )
             </h3>
             <h4 className="about">
-              - Landing the layout and
-              creation of themes on CMS WordPress
+              installation of the layout on the engine of the Wordpress site management system
+              working with plugins for WP
+              creating your own themes for Wordpress
+              principles of work with site management systems (CMS)
+              working with local servers and phpmyadmin
             </h4>
             <a href="#contact" className="contact-member">
               <span>Certificate</span>
@@ -65,11 +86,12 @@ const Members = () => {
             <h1 className="name">Course "Practical JavaScript –
               Advanced Level" </h1>
             <h3 className="position">UDEMY</h3>
-            <h3 className="period"> 10.2021 - 12.2021 (2 months)
+            <h3 className="period"> 10.2021 - 12.2021 ( 2 months )
             </h3>
-            {/*<h4 className="about">
-              -  text
-            </h4>*/}
+            <h4 className="about">
+              Сreating a JavaScript UI Library,
+              Practice with OPP style
+            </h4>
             <a href="#contact" className="contact-member">
               <span>Certificate</span>
             </a>
@@ -82,9 +104,9 @@ const Members = () => {
             <h1 className="name"> Course "Node.js"
             </h1>
             <div className="position">UDEMY</div>
-            <h3 className="period"> 06.2022 - 10.2022 (4 months) </h3>
+            <h3 className="period"> 06.2022 - 10.2022 ( 4 months ) </h3>
             <h4 className="about">
-              - from scratch,
+              from scratch,
               basics and application architecture
             </h4>
             <a href="#contact" className="contact-member">
