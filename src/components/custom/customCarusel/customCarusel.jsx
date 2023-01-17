3c5d26202fc4aab2966429413024b0ffc886f183
Serhii Burdeinyi <77573014+tibtus@users.noreply.github.com>
2023-01-17 21:54:34 +0200

    #update_files_19

--- src/components/custom/customCarusel/customCarusel.jsx
+++ src/components/custom/customCarusel/customCarusel.jsx
@@ -2,18 +2,79 @@ import React from "react"
 
 //styles
 import "./customCarusel.css";
+//MUI
+import Carousel from 'react-material-ui-carousel'
+import {Button, Paper} from '@mui/material'
+import Home from '@mui/icons-material/Home';
+
+
+function Item(props) {
+    return (
+        <Paper>
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+
+            <Button className="CheckButton">
+                Check it out!
+            </Button>
+        </Paper>
+    )
+}
 
 const CustomCarusel = () => {
+
+    let items = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #3",
+            description: "Hello World!"
+        },
+        {
+            name: "Random Name #4",
+            description: "Hello World!"
+        }
+    ]
 
 
     return (
         <div className="stylesBlock">
-            <div className="stylesBlockTop">
-                1
-            </div>
-            <div className="stylesBlockButton">
-                2
-            </div>
+            <Carousel
+                height={"100vh"}
+                NextIcon={<Home/>}
+                PrevIcon={<Home/>}
+                IndicatorIcon={<Home/>} // Previous Example
+                indicatorIconButtonProps={{
+                    style: {
+                        padding: '10px',    // 1
+                        color: 'blue'       // 3
+                    }
+                }}
+                activeIndicatorIconButtonProps={{
+                    style: {
+                        backgroundColor: 'rgba(255, 255, 255, 0.336)' // 2
+                    }
+                }}
+                indicatorContainerProps={{
+                    style: {
+                        marginTop: '50px', // 5
+                        textAlign: 'center' // 4
+                    }
+
+                }}
+            >
+                {
+                    items.map((item, i) => <Item key={i} item={item}/>)
+                }
+            </Carousel>
+
+
         </div>
 
     );
