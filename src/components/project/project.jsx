a61543db06dda27dd202210b1a79d81b757dab44
Serhii Burdeinyi <77573014+tibtus@users.noreply.github.com>
2023-01-26 20:41:28 +0200

    #update_files_customCatusel_31

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -5,16 +5,33 @@ import CustomCarusel from "../custom/customCarusel/customCarusel";
 
 
 const Project = ({className}) => {
-
     const [elements, setElements] = useState([]);
+    const [activeIndex, setActiveIndex] = useState(null);
+    const [propsClass, setPropsClass] = useState(null);
 
     useEffect(() => {
-        const divElements = document.querySelectorAll(`.${className} .option`);
+        setPropsClass(className);
+    }, [])
+
+    const divElements = document.querySelectorAll(`.${propsClass} .option`);
+
+
+    useEffect(() => {
         const elementsArray = Array.from(divElements).map(element => element.textContent);
         setElements(elementsArray);
-    }, [className]);
+    }, [divElements]);
 
-    console.log(className)
+    const handleSelect = (index) => {
+        setActiveIndex(index);
+        divElements.forEach((element, i) => {
+            if (i === index) {
+                element.classList.add("active");
+            } else {
+                element.classList.remove("active");
+            }
+        });
+    }
+
     return (
         <div style={{width: "100%"}}>
             <h1 className="member-txt">Project</h1>
@@ -23,7 +40,7 @@ const Project = ({className}) => {
 
                 <ul>
                     {elements.map((element, index) => (
-                        <li key={index}>{element} </li>
+                        <li key={index} onClick={() => handleSelect(index)}>{element}</li>
                     ))}
                 </ul>
 
