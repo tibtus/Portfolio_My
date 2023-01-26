c1f18558644518bcd5db0c3269f7b9b07ca64271
Serhii Burdeinyi <77573014+tibtus@users.noreply.github.com>
2023-01-26 22:44:50 +0200

    #update_files_customCatusel_44

--- src/components/custom/customCarusel/customCarusel.jsx
+++ src/components/custom/customCarusel/customCarusel.jsx
@@ -4,7 +4,7 @@ import React from "react"
 import "./customCarusel.css";
 //MUI
 // react icons
-import {DiGit, DiJsBadge, DiReact} from "react-icons/di";
+import {DiGit} from "react-icons/di";
 
 const CustomCarusel = () => {
     //імпорт зоображення
@@ -30,88 +30,135 @@ const CustomCarusel = () => {
 
                 <div className="options"
                 >
+
                     <div className="option active"
                          style={{"--optionBackground": `url(${images[0]}) center/cover no-repeat`}}
                         /*style={{background: `url(${bgImage}) center/cover no-repeat`}}*/
-                         onClick={handleClick}>
-                        <div className="shadow"></div>
-                        <div className="label">
-                            <DiGit className="icon"/>
-                            <div className="info">
-                                <div className="main">My WORK 1</div>
-                                <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                         onClick={handleClick}
+
+                    >
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
+
+                                <div className="info">
+                                    <div className="main">My WORK 1</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
                             </div>
-                        </div>
+                        </a>
+
+
                     </div>
 
-                <div className="option "
-                     style={{"--optionBackground": `url(${images[1]}) center/cover no-repeat`}}
-                     onClick={handleClick}
-                >
-                    <div className="shadow"></div>
-                    <div className="label">
-                        <DiReact className="icon"/>
-                        <div className="info">
-                            <div className="main">My WORK 2</div>
-                            <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="option"
-                     style={{"--optionBackground": `url(${images[2]}) center/cover no-repeat`}}
-                     onClick={handleClick}
-                >
 
-                    <div className="shadow"></div>
-                    <div className="label">
-                        <DiJsBadge className="icon"/>
-                        <div className="info">
-                            <div className="main">My WORK 3</div>
-                            <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="option"
-                     style={{"--optionBackground": `url(${images[3]}) center/cover no-repeat`}}
-                     onClick={handleClick}
-                >
-                    <div className="shadow"></div>
-                    <div className="label">
+                    <div className="option "
+                         style={{"--optionBackground": `url(${images[1]}) center/cover no-repeat`}}
+                         onClick={handleClick}
+                    >
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
 
-                        <DiReact className="icon"/>
-                        <div className="info">
-                            <div className="main">My WORK 4</div>
-                            <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
-                        </div>
+                                <div className="info">
+                                    <div className="main">My WORK 2</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
+                            </div>
+                        </a>
                     </div>
-                </div>
-                <div className="option"
-                     style={{"--optionBackground": `url(${images[4]}) center/cover no-repeat`}}
-                     onClick={handleClick}
-                >
-                    <div className="shadow"></div>
-                    <div className="label">
-                        <DiJsBadge className="icon"/>
-                        <div className="info">
-                            <div className="main">My WORK 5</div>
-                            <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
-                        </div>
+                    <div className="option"
+                         style={{"--optionBackground": `url(${images[2]}) center/cover no-repeat`}}
+                         onClick={handleClick}
+                    >
+
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
+
+                                <div className="info">
+                                    <div className="main">My WORK 3</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
+                            </div>
+                        </a>
                     </div>
-                </div>
+                    <div className="option"
+                         style={{"--optionBackground": `url(${images[3]}) center/cover no-repeat`}}
+                         onClick={handleClick}
+                    >
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
+
+                                <div className="info">
+                                    <div className="main">My WORK 4</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div>
+                    <div className="option"
+                         style={{"--optionBackground": `url(${images[4]}) center/cover no-repeat`}}
+                         onClick={handleClick}
+                    >
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
+
+                                <div className="info">
+                                    <div className="main">My WORK 5</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div>
 
 
                     <div className="option"
                          style={{"--optionBackground": `url(${images[0]}) center/cover no-repeat`}}
                          onClick={handleClick}
                     >
-                        <div className="shadow"></div>
-                        <div className="label">
-                            <DiReact className="icon"/>
-                            <div className="info">
-                                <div className="main">My WORK 6</div>
-                                <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
+
+                                <div className="info">
+                                    <div className="main">My WORK 6</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
                             </div>
-                        </div>
+                        </a>
                     </div>
 
 
@@ -119,14 +166,21 @@ const CustomCarusel = () => {
                          style={{"--optionBackground": `url(${images[1]}) center/cover no-repeat`}}
                          onClick={handleClick}
                     >
-                        <div className="shadow"></div>
-                        <div className="label">
-                            <DiJsBadge className="icon"/>
-                            <div className="info">
-                                <div className="main">My WORK 7</div>
-                                <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                        <a
+                            href="https://google.com"
+                            target={"blank"}
+                        >
+                            <div className="shadow"></div>
+                            <div className="label">
+                                <DiGit className="icon"/>
+
+                                <div className="info">
+                                    <div className="main">My WORK 7</div>
+                                    <div className="sub">TEXT TEXT TEXT TEXT TEXT</div>
+                                </div>
+
                             </div>
-                        </div>
+                        </a>
                     </div>
 
 
