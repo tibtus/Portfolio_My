22475839c57e072df440d252f0f8b5e8bf040a0d
Serhii Burdeinyi <77573014+tibtus@users.noreply.github.com>
2023-01-26 16:53:31 +0200

    #update_files_25

--- src/components/custom/customCarusel/customCarusel.jsx
+++ src/components/custom/customCarusel/customCarusel.jsx
@@ -7,12 +7,17 @@ import "./customCarusel.css";
 const CustomCarusel = () => {
 
 
-    return (
-        <div className="stylesBlock">
+    /* $(".option").click(function(){
+         $(".option").removeClass("active");
+         $(this).addClass("active");
 
+     });*/
+
+
+    return (
+        <div className="bodyBlock">
             <div className="options">
-                <div className="option active"
-                     style="--optionBackground:url(https://66.media.tumblr.com/6fb397d822f4f9f4596dff2085b18f2e/tumblr_nzsvb4p6xS1qho82wo1_1280.jpg);">
+                <div className="option active">
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
@@ -24,8 +29,7 @@ const CustomCarusel = () => {
                         </div>
                     </div>
                 </div>
-                <div className="option"
-                     style="--optionBackground:url(https://66.media.tumblr.com/8b69cdde47aa952e4176b4200052abf4/tumblr_o51p7mFFF21qho82wo1_1280.jpg);">
+                <div className="option">
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
@@ -37,8 +41,7 @@ const CustomCarusel = () => {
                         </div>
                     </div>
                 </div>
-                <div className="option"
-                     style="--optionBackground:url(https://66.media.tumblr.com/5af3f8303456e376ceda1517553ba786/tumblr_o4986gakjh1qho82wo1_1280.jpg);">
+                <div className="option">
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
@@ -50,8 +53,7 @@ const CustomCarusel = () => {
                         </div>
                     </div>
                 </div>
-                <div className="option"
-                     style="--optionBackground:url(https://66.media.tumblr.com/5516a22e0cdacaa85311ec3f8fd1e9ef/tumblr_o45jwvdsL11qho82wo1_1280.jpg);">
+                <div className="option">
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
@@ -64,7 +66,7 @@ const CustomCarusel = () => {
                     </div>
                 </div>
                 <div className="option"
-                     style="--optionBackground:url(https://66.media.tumblr.com/f19901f50b79604839ca761cd6d74748/tumblr_o65rohhkQL1qho82wo1_1280.jpg);">
+                     style={{color: "red"}}>
                     <div className="shadow"></div>
                     <div className="label">
                         <div className="icon">
